d3bb47e5e3d298beb1cd768f6738cd2c56b03ec8
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-12-04 19:34:04 +0900

    add urdf scripts for puzzle_blocks

---
+++ scripts/make_puzzle_env.py
@@ -0,0 +1,23 @@
+###
+exec(open('/choreonoid_ws/install/share/irsl_choreonoid/sample/irsl_import.py').read())
+
+lst = (
+  { "pos": [-0.03,  0.1,  0.046], "aa": [0, 1, 0, 3.141592653589793], "name": 'yellow' },
+  { "pos": [ 0.1,   0.1,  0.016], "name": 'red' },
+  { "pos": [ 0.13,  0.0,  0.046], "aa": [0, 1, 0, 3.141592653589793], "name": 'purple' },
+  { "pos": [ 0.1,  -0.1,  0.046], "aa": [0, 1, 0, 3.141592653589793], "name": 'lightgreen' },
+  { "pos": [-0.03, -0.13, 0.046], "aa": [0, 1, 0, 3.141592653589793], "name": 'green' },
+  { "pos": [ 0.03,  0.2,  0.046], "aa": [0, 1, 0, 3.141592653589793], "name": 'cyan'  },
+  { "pos": [ 0.03, -0.2,  0.016], "name": 'brown' }
+  )
+offset=coordinates(npa([0.3, 0, 0]))
+
+## spawn urdf
+for l in lst:
+  name = l['name']
+  cds = ru.make_coordinates(l)
+  cds.transform(offset, coordinates.wrt.world)
+  pos = cds.pos; RPY = cds.getRPY()
+  xx = pos[0]; yy = pos[1]; zz = pos[2]
+  RR = RPY[0]; PP = RPY[1]; YY = RPY[2]
+  print('rosrun gazebo_ros spawn_model -urdf -file $(rospack find irsl_sim_environments)/urdf/puzzle_blocks/{}_block/{}_block.urdf \\\n -model {}_block -x {} -y {} -z {} -R {} -P {} -Y {}'.format(name, name, name, xx, yy, zz, RR, PP, YY))
---
+++ urdf/puzzle_blocks/spawn_scripts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rosrun gazebo_ros spawn_model -urdf -file $(rospack find irsl_sim_environments)/urdf/puzzle_blocks/puzzle_base.urdf \
+ -model puzzle_base -x 0.3
+
+rosrun gazebo_ros spawn_model -urdf -file $(rospack find irsl_sim_environments)/urdf/puzzle_blocks/yellow_block/yellow_block.urdf \
+ -model yellow_block -x 0.27 -y 0.1 -z 0.046 -R 3.141592653589793 -P 1.2246467991473532e-16 -Y 3.141592653589793
+
+rosrun gazebo_ros spawn_model -urdf -file $(rospack find irsl_sim_environments)/urdf/puzzle_blocks/red_block/red_block.urdf \
+ -model red_block -x 0.4 -y 0.1 -z 0.016 -R 0.0 -P -0.0 -Y 0.0
+
+rosrun gazebo_ros spawn_model -urdf -file $(rospack find irsl_sim_environments)/urdf/puzzle_blocks/purple_block/purple_block.urdf \
+ -model purple_block -x 0.43 -y 0.0 -z 0.046 -R 3.141592653589793 -P 1.2246467991473532e-16 -Y 3.141592653589793
+
+rosrun gazebo_ros spawn_model -urdf -file $(rospack find irsl_sim_environments)/urdf/puzzle_blocks/lightgreen_block/lightgreen_block.urdf \
+ -model lightgreen_block -x 0.4 -y -0.1 -z 0.046 -R 3.141592653589793 -P 1.2246467991473532e-16 -Y 3.141592653589793
+
+rosrun gazebo_ros spawn_model -urdf -file $(rospack find irsl_sim_environments)/urdf/puzzle_blocks/green_block/green_block.urdf \
+ -model green_block -x 0.27 -y -0.13 -z 0.046 -R 3.141592653589793 -P 1.2246467991473532e-16 -Y 3.141592653589793
+
+rosrun gazebo_ros spawn_model -urdf -file $(rospack find irsl_sim_environments)/urdf/puzzle_blocks/cyan_block/cyan_block.urdf \
+ -model cyan_block -x 0.32999999999999996 -y 0.2 -z 0.046 -R 3.141592653589793 -P 1.2246467991473532e-16 -Y 3.141592653589793
+
+rosrun gazebo_ros spawn_model -urdf -file $(rospack find irsl_sim_environments)/urdf/puzzle_blocks/brown_block/brown_block.urdf \
+ -model brown_block -x 0.32999999999999996 -y -0.2 -z 0.016 -R 0.0 -P -0.0 -Y 0.0
